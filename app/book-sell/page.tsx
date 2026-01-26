e69642ba02bdae089bd871969f7517c94a676d5a
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-01-26 19:43:08 +0530

    feat: Add common TypeScript interfaces and initialize book selling page with form and state management.

--- app/book-sell/page.tsx
+++ app/book-sell/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BookDetails } from "@/lib/types/type";
+import { useAddProductsMutation } from "@/store/api";
+import { RootState } from "@/store/store";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+  const [addProducts] = useAddProductsMutation();
 
-export default page
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.user.user);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BookDetails>();
+  return <div>page</div>;
+};
+
+export default page;
